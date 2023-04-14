e85a1305eb0d5835d513b33e8f76f0c738bde628
Samhita Alla <aallasamhita@gmail.com>
2023-04-14 22:52:15 +0530

    axios
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,7 @@ import App from "./App.vue";
 import router from "./router";
 
 axios.defaults.withCredentials = true;
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 axios.defaults.baseURL = process.env.VUE_APP_FASTAPI_URL; // the FastAPI backend
 
 Vue.createApp(App).use(router).mount("#app");
